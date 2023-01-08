0cf44a1626e6d102e50ac4dab01f51e8a0954ed3
Athirson <athirsonarceus@gmail.com>
2023-01-08 15:54:19 -0300

    feat: changing logo name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Col, Container, Row } from 'react-bootstrap'
-import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
@@ -10,7 +9,7 @@ export const Footer = () => {
 			<Container>
 				<Row className='align-item-center'>
 					<Col sm={6}>
-						<img src={logo} alt='logo'></img>
+						<h1 className='brand-name'>Athirson</h1>
 					</Col>
 					<Col sm={6} className='text-center text-sm-end'>
 						<div className='social-icon'>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
-import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
@@ -34,7 +33,7 @@ export const NavBar = () => {
 			<Navbar expand='md' className={scrolled ? 'scrolled' : ''}>
 				<Container>
 					<Navbar.Brand href='/'>
-						<img src={logo} alt='Logo' />
+						<h1 className='brand-name'>Athirson</h1>
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav'>
 						<span className='navbar-toggler-icon'></span>
