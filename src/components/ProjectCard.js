bac564a4c8ef592831d73bf6f7e3319a6e577cc9
Athirson <athirsonarceus@gmail.com>
2023-01-08 16:04:00 -0300

    feat: capiche

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,12 @@
 import { Col } from 'react-bootstrap'
 
-export const ProjectCard = ({ name, logo, description, tech, github }) => {
+export const ProjectCard = ({ name, logo, description, github }) => {
 	return (
 		<Col sm={6} md={4}>
-			<div className='proj-imgbx'>
+			<div
+				className='proj-imgbx'
+				onClick={() => window.open(github, '_blank')}
+			>
 				<img
 					src={`https://img.icons8.com/color/48/000000/${logo}.svg`}
 					alt='project'
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,7 +2,6 @@ export const backendProjects = [
 	{
 		name: 'Task Manager API',
 		description: 'A webservice to create and manage tasks',
-		tech: ['TypeScript', 'Node.js', 'NestJS', 'Postgres'],
 		logo: 'nestjs',
 		github: 'https://github.com/AthirsonSilva/task-manager'
 	},
@@ -10,7 +9,6 @@ export const backendProjects = [
 		name: 'Car pricing API',
 		description:
 			'Car pricing evaluator API using TypeScript, Node.js.js, NestJS and TypeORM',
-		tech: ['TypeScript', 'Node.js', 'NestJS', 'SQLite'],
 		logo: 'nestjs',
 		github: 'https://github.com/AthirsonSilva/car-pricing-api'
 	},
@@ -18,7 +16,6 @@ export const backendProjects = [
 		name: 'Next-Meal API',
 		description:
 			"A Node.js and NestJS version of my technician in Systems development course conclusion project's back-end, that was made in Laravel.",
-		tech: ['TypeScript', 'Node.js', 'NestJS', 'MySQL'],
 		logo: 'nestjs',
 		github: 'https://github.com/AthirsonSilva/next-meal-api'
 	},
@@ -26,21 +23,18 @@ export const backendProjects = [
 		name: 'Bitcoin Price tracker',
 		description:
 			'A simple webpage that displays the current Bitcoin price in various currencies',
-		tech: ['TypeScript', 'Deno.js', 'Fresh'],
 		logo: 'deno',
 		github: 'https://github.com/AthirsonSilva/bitcoin-tracker'
 	},
 	{
 		name: 'Notifications microservice',
 		description: 'A notification sender and manager microservice',
-		tech: ['JavaScript', 'Node.js', 'NestJS', 'Postgres'],
 		logo: 'nestjs',
 		github: 'https://github.com/AthirsonSilva/notifications-service'
 	},
 	{
 		name: 'School API',
 		description: 'A webservice for creating and assign lessons to students',
-		tech: ['TypeScript', 'Node.js', 'Express', 'MariaDB'],
 		logo: 'express',
 		github: 'https://github.com/AthirsonSilva/school-api'
 	}
@@ -51,7 +45,6 @@ export const frontendProjects = [
 		name: 'Next-Meal App',
 		description:
 			'App to find and make restaurant reservations through geolocation.',
-		tech: ['TypeScript', 'React', 'React-Native', 'Expo'],
 		logo: 'react-native',
 		github: 'https://github.com/AthirsonSilva/next-meal-app'
 	},
@@ -59,7 +52,6 @@ export const frontendProjects = [
 		name: 'PokeAPI',
 		description:
 			'A simple React.js web app to display random pokemon data using the PokeAPI',
-		tech: ['React', 'JavaScript', 'CSS'],
 		logo: 'react-native',
 		github: 'https://github.com/AthirsonSilva/react-pokeapi'
 	},
@@ -67,7 +59,6 @@ export const frontendProjects = [
 		name: 'QRCode Scanner',
 		description:
 			"A simple mobile application and web service to generate and scan QRcode values to be used as entry control for Etec's future events.",
-		tech: ['React', 'React-Native'],
 		logo: 'react-native',
 		github: 'https://github.com/AthirsonSilva/qrcode-scanner'
 	},
@@ -75,14 +66,12 @@ export const frontendProjects = [
 		name: 'Pac-man Game',
 		description:
 			'A simple Pac-man mimic game with vanilla JavaScript using the concepts of OOP. The game is pretty much done, i can still add more features and upgrade the game at some point, but it will not be done anytime soon.',
-		tech: ['JavaScript', 'HTML', 'CSS'],
 		logo: 'javascript',
 		github: 'https://github.com/AthirsonSilva/Pac-man'
 	},
 	{
 		name: 'Agenda Project',
 		description: 'A simple agenda app made with EJS and Node.js',
-		tech: ['Node.js', 'Express', 'MySQL', 'EJS', 'CSS'],
 		logo: 'express',
 		github: 'https://github.com/AthirsonSilva/agenda-project'
 	},
@@ -90,7 +79,6 @@ export const frontendProjects = [
 		name: 'Shooter Game',
 		description:
 			'A simple shooter game i did with vanilla JavaScript, asap and Tailwind. In this one i added some cool frameworks to make the animation, particles and to adjust the game layout.',
-		tech: ['JavaScript', 'Tailwind', 'Asap', 'GSAP'],
 		logo: 'javascript',
 		github: 'https://github.com/AthirsonSilva/Shooter-game'
 	}
@@ -101,7 +89,6 @@ export const dataScienceProjects = [
 		name: 'Fandango Ratings',
 		description:
 			'A project made for educational purposes to find out if the website Fandango artificially increases the movies ratings and stars to sell more tickets.',
-		tech: ['Python', 'Pandas', 'Numpy', 'Matplotlib', 'Seaborn'],
 		logo: 'python',
 		github: 'https://github.com/AthirsonSilva/fandango-ratings'
 	},
@@ -110,52 +97,33 @@ export const dataScienceProjects = [
 		description:
 			'A simple artificial intelligence and machine-learning introductory project made from a online tutorial. It receives data from a .csv file and creates a AI model that can predict the musical taste of a person from their gender and age.',
 		logo: 'python',
-		github: 'https://github.com/AthirsonSilva/Music-recommender',
-		tech: ['Python', 'Pandas', 'Numpy', 'Matplotlib', 'Seaborn']
+		github: 'https://github.com/AthirsonSilva/Music-recommender'
 	},
 	{
 		name: 'Radiology Artificial Intelligence',
 		description:
 			'Predicts if someone has a high risk of dying by heart attack or related diseases',
 		logo: 'python',
-		github: 'https://github.com/AthirsonSilva/radiology_artificial_intelligence',
-		tech: ['Python', 'Pandas', 'Numpy', 'Matplotlib', 'Seaborn']
+		github: 'https://github.com/AthirsonSilva/radiology_artificial_intelligence'
 	},
 	{
 		name: 'Intesivão Python',
 		description:
 			'Repository to storage the project and files made in the course "Intesivão Python" by Hashtangs Treinamentos.',
 		logo: 'python',
-		github: 'https://github.com/AthirsonSilva/intesivao-python',
-		tech: [
-			'Python',
-			'Pandas',
-			'Numpy',
-			'Matplotlib',
-			'Seaborn',
-			'Scikit-learn'
-		]
+		github: 'https://github.com/AthirsonSilva/intesivao-python'
 	},
 	{
 		name: 'Machine Learning Masterclass',
 		description:
 			'All files from the Udemy "Machine-Learning Masterclass" Python and Data science course',
 		logo: 'python',
-		github: 'https://github.com/AthirsonSilva/machine-learning-masterclass',
-		tech: [
-			'Python',
-			'Pandas',
-			'Numpy',
-			'Matplotlib',
-			'Seaborn',
-			'Scikit-learn'
-		]
+		github: 'https://github.com/AthirsonSilva/machine-learning-masterclass'
 	},
 	{
 		name: 'Excel Python Integration',
 		description: 'A simple python script to integrate excel files',
 		logo: 'python',
-		github: 'https://github.com/AthirsonSilva/excel_python_integration',
-		tech: ['Python', 'Pandas', 'Numpy']
+		github: 'https://github.com/AthirsonSilva/excel_python_integration'
 	}
 ]
